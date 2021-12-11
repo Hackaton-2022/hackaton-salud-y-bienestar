1fef38bcc1f6d8c52f1ca7702fe2d866e0552194
Juan Motta <j_andreslm@hotmail.com>
2021-12-11 14:36:59 -0500

    navbar

---
+++ src/components/navbar/NavbarComponent.vue
@@ -0,0 +1,121 @@
+<template>
+	<div class="row navbar">
+		<div class="col-sm-12 col-md-8 ">
+			<div class="row">
+				<div class="col-sm-12 col-md-3 col-lg-3 mt-3  navbar-title">
+					<span>
+						<router-link :to="{ name: 'Home'}">APP</router-link>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div
+			class="col-sm-12 col-md-4 mt-3"
+			v-if="isLoged"
+		>
+			<div class="row">
+				<div
+					class="col-sm-12 col-md-6 col-lg-7 navbar-name"
+					v-on:click="toProfile"
+				>
+					<b-icon-person-circle
+						scale="2"
+						style="margin-right: 20px"
+					></b-icon-person-circle>
+					<p>
+						{{ name }}
+					</p>
+				</div>
+				<div class="col-sm-12 col-md-5 col-lg-4 offset-md-1 offset-lg-1 navbar-button mt-3 mb-3">
+					<div v-on:click="setLogout">
+						<router-link :to="{ name: 'Home'}">
+							Cerrar sesion
+						</router-link>
+
+					</div>
+				</div>
+			</div>
+		</div>
+		<div
+			class="col-sm-12 col-md-4"
+			v-else
+		>
+			<div class="row">
+				<div class="col-sm-12 col-md-5 col-lg-4 offset-md-1 offset-lg-2 navbar-button mt-3">
+					<router-link :to="{ name: 'Login'}">Iniciar Sesion</router-link>
+				</div>
+				<div class="col-sm-12 col-md-5 col-lg-4 offset-md-1 offset-lg-2 navbar-button mt-3">
+					<router-link :to="{ name: 'CreateUser'}">Registro</router-link>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	.borde {
+		border: 1px solid black;
+	}
+	.navbar {
+		margin: 15px 0;
+	}
+	.navbar-title {
+		font-size: 30px;
+		font-weight: bold;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+	}
+	.navbar-title span a {
+		text-decoration: none;
+		color: black;
+	}
+	.navbar-route {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.navbar-route span {
+		cursor: pointer;
+	}
+	.navbar-route span a {
+		text-decoration: none;
+		color: black;
+	}
+	.navbar-route span a:hover {
+		text-decoration: none;
+		color: #006095;
+	}
+	.navbar-button {
+		font-weight: bold;
+		color: white;
+		background-color: #006095;
+		border-radius: 5px;
+		padding: 3px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+	}
+	.navbar-button:hover {
+		background-color: #007c9c;
+	}
+	.navbar-button a {
+		text-decoration: none;
+		color: white;
+	}
+	.navbar-name {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.navbar-name p {
+		font-size: 20px;
+		margin-top: 15px;
+	}
+	.navbar-name:hover {
+		color: #006095;
+		cursor: pointer;
+	}
+</style>
---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>Home view</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home',
+		components: {
+		}
+	}
+</script>
