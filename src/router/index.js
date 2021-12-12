418651dc148d2499fc6721f647583e90c07980e3
Juan Motta <j_andreslm@hotmail.com>
2021-12-12 11:47:03 -0500

    registro y login maquetados

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/HomeView.vue'
+import Register from '../views/Register/RegisterView.vue';
+import Login from '../views/Register/LoginView.vue';
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/registro',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/calculadora',
---
+++ src/views/Register/RegisterView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<RegisterComponent />
+	</div>
+</template>
+
+<script>
+	import RegisterComponent from '../../components/register/RegisterComponent.vue'
+	export default {
+		name: 'Register',
+		components: {
+			RegisterComponent,
+		}
+	}
+</script>
